213c4d8329f0188bed0ca677a41155aef4895b68
MarkusTryban <markustryban@gmail.com>
2019-09-28 19:28:23 -0400

    add bcrypt to salt user password

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs')
 
 const router = express.Router()
 const { check, validationResult } = require('express-validator')
@@ -46,6 +47,12 @@ router.post(
         avatar,
         password
       })
+
+      const salt = await bcrypt.genSalt(10)
+
+      user.password = await bcrypt.hash(password, salt)
+
+      await user.save()
 
       res.send('User route')
     } catch (err) {
