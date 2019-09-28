50ecfec20263a937cc02f5aa64d8f29bcd8dc261
MarkusTryban <markustryban@gmail.com>
2019-09-28 19:01:00 -0400

    add try catch to router.post

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const express = require('express')
 const router = express.Router()
 const { check, validationResult } = require('express-validator')
 
+const User = require('../../models/User')
+
 router.post(
   '/',
   [
@@ -15,14 +17,21 @@ router.post(
       'Please use a password that is a minimum of 6 char'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req)
 
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() })
     }
 
-    res.send('User route')
+    const { name, email, password } = req.body
+
+    try {
+      res.send('User route')
+    } catch (err) {
+      console.log(err.message)
+      res.status(500).send('Server error')
+    }
   }
 )
 
