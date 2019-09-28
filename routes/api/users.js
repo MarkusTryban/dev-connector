b38f038a5118a9dad7c9c81554767ed13d0b7097
MarkusTryban <markustryban@gmail.com>
2019-09-28 16:41:37 -0400

    add check & validationResult to users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,29 @@
 const express = require('express')
 
 const router = express.Router()
+const { check, validationResult } = require('express-validator')
 
-router.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('User route')
-})
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please use a password that is a minimum of 6 char'
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    res.send('User route')
+  }
+)
 
 module.exports = router
