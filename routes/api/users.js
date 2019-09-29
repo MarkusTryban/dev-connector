42cb4744aa63f13a6756cbb670eb8910ae981115
MarkusTryban <markustryban@gmail.com>
2019-09-29 10:44:39 -0400

    add jwt & config

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 
 const router = express.Router()
 const { check, validationResult } = require('express-validator')
@@ -55,6 +57,22 @@ router.post(
       user.password = await bcrypt.hash(password, salt)
 
       await user.save()
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+
+      jwt.sign(
+        payload,
+        config.get('jwtToken'),
+        { expiretIn: 360000 },
+        (err, token) => {
+          if (err) throw err
+          res.json({ token })
+        }
+      )
 
       res.send('User route')
     } catch (err) {
