86ca6bd57295e8c15e7de1dec91df7a6099717a3
MarkusTryban <markustryban@gmail.com>
2019-09-29 21:01:03 -0400

    add router.get

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -99,4 +99,14 @@ router.post(
   }
 )
 
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar'])
+    res.json(profiles)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server error')
+  }
+})
+
 module.exports = router
