31817680eaef251d4bf26fa0dd1a46ddc7174310
MarkusTryban <markustryban@gmail.com>
2019-10-03 19:10:54 -0400

    create post model in Post.js

---
+++ models/Post.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  }
+})
