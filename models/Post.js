acddd850e0af89508984d8c0ec6315fecee0e0c4
MarkusTryban <markustryban@gmail.com>
2019-10-03 19:20:51 -0400

    add likes, comments & date to user posts

--- models/Post.js
+++ models/Post.js
@@ -14,5 +14,41 @@ const PostSchema = new Schema({
   },
   avatar: {
     type: String
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
   }
 })
+
+module.exports = mongoose.model('post', PostSchema)
