896a644ebe667ed693dc9b0f82ee5f166c3415b9
MarkusTryban <markustryban@gmail.com>
2019-10-03 21:43:38 -0400

    bring in Post, Profile & User models

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,8 +1,12 @@
 const express = require('express')
+
+const router = express.Router()
 const { check, validationResult } = require('express-validator')
 const auth = require('../../middleware/auth')
 
-const router = express.Router()
+const Post = require('../../models/Post')
+const Profile = require('../../models/Profile')
+const User = require('../../models/User')
 
 router.post(
   '/',
