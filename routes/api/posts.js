84ba882b7090eeb724239dbfeb8d2fba7e571ab9
MarkusTryban <markustryban@gmail.com>
2019-10-03 21:27:10 -0400

    bring in check, validationResult & auth

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const { check, validationResult } = require('express-validator')
+const auth = require('../../middleware/auth')
 
 const router = express.Router()
 
