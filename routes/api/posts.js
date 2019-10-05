699eddd46f81a4d942903610b8e9279e312e866e
MarkusTryban <markustryban@gmail.com>
2019-10-05 11:23:40 -0400

    Check if post has already been liked by user

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -104,6 +104,12 @@ router.delete('/:id', auth, async (req, res) => {
 router.put('/like/:id', auth, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id)
+
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length > 0
+    ) {
+      return res.json(400).json({ msg: 'Post already liked' })
+    }
   } catch (err) {
     console.error(err.message)
     res.status(500).send('Server error')
