89bdec90654ba076c4b9de9515a3911bf2b5cf71
MarkusTryban <markustryban@gmail.com>
2019-10-05 00:03:39 -0400

    add get request to find a post by the id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -55,4 +55,19 @@ router.get('/', auth, async (req, res) => {
   }
 })
 
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById()
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' })
+    }
+
+    res.json(post)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server error')
+  }
+})
+
 module.exports = router
