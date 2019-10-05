7a4c6a1bbde80a1b5f55ffcd658fa00e8fd11580
MarkusTryban <markustryban@gmail.com>
2019-10-05 14:37:40 -0400

    Check if user is authorized to delete post comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -198,6 +198,10 @@ router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
     if (!comment) {
       return res.status(404).json({ msg: 'Comment does not exist' })
     }
+
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' })
+    }
   } catch (err) {
     console.error(err.message)
     res.status(500).send('Sever error')
