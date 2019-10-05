a17848178b54438035dde26559cf580f24387c4b
MarkusTryban <markustryban@gmail.com>
2019-10-04 20:34:12 -0400

    add the newPost object to a try catch with error msg

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -24,13 +24,18 @@ router.post(
       return res.status(400).json({ errors: errors.array() })
     }
 
-    const user = await User.findById(req.user.id).select('-passward')
+    try {
+      const user = await User.findById(req.user.id).select('-passward')
 
-    const newPost = {
-      text: req.body.test,
-      name: user.name,
-      avatar: user.avatar,
-      user: req.user.id
+      const newPost = {
+        text: req.body.test,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      }
+    } catch (err) {
+      console.error(err.message)
+      res.status(500).send('Server error')
     }
   }
 )
