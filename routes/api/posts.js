0f2f019a2ff934ea8dbb81dbc0ca92b1a3c7d22a
MarkusTryban <markustryban@gmail.com>
2019-10-05 09:22:54 -0400

    Check if post is not found & throw error msg

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -79,6 +79,10 @@ router.delete('/:id', auth, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id)
 
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' })
+    }
+
     if (post.user.toString() !== req.user.id) {
       return res.status(401).json({ msg: 'User not authorized' })
     }
