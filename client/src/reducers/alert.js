75a3468c1d6a83b5ca77a7023969c264aa73f55a
MarkusTryban <markustryban@gmail.com>
2019-10-07 19:07:33 -0400

    Filter through array & remove alert by id'

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -6,5 +6,9 @@ export default function(state = initialState, action) {
   switch (action.type) {
     case SET_ALERT:
       return [...state, action.payload]
+    case REMOVE_ALERT:
+      return state.filter(alert => alert.id !== payload)
+    default:
+      return state
   }
 }
