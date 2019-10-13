8dc83722e1d068876f388b19008af22b440deb10
MarkusTryban <markustryban@gmail.com>
2019-10-13 19:50:20 -0400

    Add useEffect to dispatch loadUser

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
@@ -9,9 +9,9 @@ import Login from './components/auth/Login'
 import Alert from './components/layout/Alert'
 import store from './store'
 import { loadUser } from './actions/auth'
+import setAuthToken from './utils/setAuthToken'
 
 import './App.css'
-import setAuthToken from './utils/setAuthToken'
 
 // eslint-disable-next-line no-undef
 if (localStorage.token) {
@@ -19,22 +19,28 @@ if (localStorage.token) {
   setAuthToken(localStorage.token)
 }
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" component={Landing} />
-        <section className="container">
-          <Alert />
-          <Switch>
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
-  </Provider>
-)
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser())
+  }, [])
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <section className="container">
+            <Alert />
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  )
+}
 
 export default App
