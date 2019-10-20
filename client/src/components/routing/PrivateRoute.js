10f564cf577840ec074fabb44c2c89bc5a34d08f
MarkusTryban <markustryban@gmail.com>
2019-10-20 16:39:14 -0400

    Fix typos

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -3,9 +3,22 @@ import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-const PrivateRoute = ({ component: Component, auth: { isAuthenticated, loading}, ...rest }) => (
-  <Route {..rest} render={props => !isAuthenticated && !loading ? (<Redirect to='/login' />) : (<Component {..props} />)} />
-) 
+const PrivateRoute = ({
+  component: Component,
+  auth: { isAuthenticated, loading },
+  ...rest
+}) => (
+  <Route
+    {...rest}
+    render={props =>
+      !isAuthenticated && !loading ? (
+        <Redirect to="/login" />
+      ) : (
+        <Component {...props} />
+      )
+    }
+  />
+);
 
 PrivateRoute.propTypes = {
   auth: PropTypes.object.isRequired
