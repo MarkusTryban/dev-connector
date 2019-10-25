be74b638067a49e8b9bb3fc51a7a7a5748a36f4d
MarkusTryban <markustryban@gmail.com>
2019-10-25 12:16:00 -0400

    Create or update user profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -18,3 +18,25 @@ export const getCurrentProfile = () => async dispatch => {
     });
   }
 };
+
+// Create or update profile
+export const createProfile = (
+  formData,
+  history,
+  edit = false
+) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    const res = await axios.post('/api/profile', formData, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+  } catch (err) {}
+};
