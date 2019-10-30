27f9dbbeee528256dfce9b21ee9d85b2c8a04f5a
MarkusTryban <markustryban@gmail.com>
2019-10-30 18:31:46 -0400

    Prettier formatting

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,11 @@
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
-const { check, validationResult } = require('express-validator')
-const auth = require('../../middleware/auth')
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
 
-const Post = require('../../models/Post')
-const User = require('../../models/User')
+const Post = require('../../models/Post');
+const User = require('../../models/User');
 
 router.post(
   '/',
@@ -18,134 +18,134 @@ router.post(
     ]
   ],
   async (req, res) => {
-    const errors = validationResult(req)
+    const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() })
+      return res.status(400).json({ errors: errors.array() });
     }
 
     try {
-      const user = await User.findById(req.user.id).select('-passward')
+      const user = await User.findById(req.user.id).select('-passward');
 
       const newPost = new Post({
         text: req.body.text,
         name: user.name,
         avatar: user.avatar,
         user: req.user.id
-      })
+      });
 
-      const post = await newPost.save()
+      const post = await newPost.save();
 
-      res.json(post)
+      res.json(post);
     } catch (err) {
-      console.error(err.message)
-      res.status(500).send('Server error')
+      console.error(err.message);
+      res.status(500).send('Server error');
     }
   }
-)
+);
 
 router.get('/', auth, async (req, res) => {
   try {
-    const posts = await Post.find().sort({ date: -1 })
+    const posts = await Post.find().sort({ date: -1 });
 
-    res.json(posts)
+    res.json(posts);
   } catch (err) {
-    console.error(err.message)
-    res.status(500).send('Server error')
+    console.error(err.message);
+    res.status(500).send('Server error');
   }
-})
+});
 
 router.get('/:id', auth, async (req, res) => {
   try {
-    const post = await Post.findById(req.params.id)
+    const post = await Post.findById(req.params.id);
 
     if (!post) {
-      return res.status(404).json({ msg: 'Post not found' })
+      return res.status(404).json({ msg: 'Post not found' });
     }
 
-    res.json(post)
+    res.json(post);
   } catch (err) {
-    console.error(err.message)
+    console.error(err.message);
 
     if (err.kind === 'ObjectId') {
-      return res.status(404).json({ msg: 'Post not found' })
+      return res.status(404).json({ msg: 'Post not found' });
     }
 
-    res.status(500).send('Server error')
+    res.status(500).send('Server error');
   }
-})
+});
 
 router.delete('/:id', auth, async (req, res) => {
   try {
-    const post = await Post.findById(req.params.id)
+    const post = await Post.findById(req.params.id);
 
     if (!post) {
-      return res.status(404).json({ msg: 'Post not found' })
+      return res.status(404).json({ msg: 'Post not found' });
     }
 
     if (post.user.toString() !== req.user.id) {
-      return res.status(401).json({ msg: 'User not authorized' })
+      return res.status(401).json({ msg: 'User not authorized' });
     }
 
-    await post.remove()
+    await post.remove();
 
-    res.json({ msg: 'Post has been removed' })
+    res.json({ msg: 'Post has been removed' });
   } catch (err) {
-    console.error(err.message)
+    console.error(err.message);
 
     if (err.kind === 'ObjectId') {
-      return res.status(404).json({ msg: 'Post not found' })
+      return res.status(404).json({ msg: 'Post not found' });
     }
 
-    res.status(500).send('Server error')
+    res.status(500).send('Server error');
   }
-})
+});
 
 router.put('/like/:id', auth, async (req, res) => {
   try {
-    const post = await Post.findById(req.params.id)
+    const post = await Post.findById(req.params.id);
 
     if (
       post.likes.filter(like => like.user.toString() === req.user.id).length > 0
     ) {
-      return res.status(400).json({ msg: 'Post already liked' })
+      return res.status(400).json({ msg: 'Post already liked' });
     }
 
-    post.likes.unshift({ user: req.user.id })
+    post.likes.unshift({ user: req.user.id });
 
-    await post.save()
+    await post.save();
 
-    res.json(post.likes)
+    res.json(post.likes);
   } catch (err) {
-    console.error(err.message)
-    res.status(500).send('Server error')
+    console.error(err.message);
+    res.status(500).send('Server error');
   }
-})
+});
 
 router.put('/unlike/:id', auth, async (req, res) => {
   try {
-    const post = await Post.findById(req.params.id)
+    const post = await Post.findById(req.params.id);
 
     if (
       post.likes.filter(like => like.user.toString() === req.user.id).length ===
       0
     ) {
-      return res.status(400).json({ msg: 'Post has not been liked' })
+      return res.status(400).json({ msg: 'Post has not been liked' });
     }
 
     const removeIndex = post.likes
       .map(like => like.user.toString())
-      .indexOf(req.user.id)
+      .indexOf(req.user.id);
 
-    post.likes.splice(removeIndex, 1)
+    post.likes.splice(removeIndex, 1);
 
-    await post.save()
+    await post.save();
 
-    res.json(post.likes)
+    res.json(post.likes);
   } catch (err) {
-    console.error(err.message)
-    res.status(500).send('Server error')
+    console.error(err.message);
+    res.status(500).send('Server error');
   }
-})
+});
 
 router.post(
   '/comment/:id',
@@ -158,63 +158,63 @@ router.post(
     ]
   ],
   async (req, res) => {
-    const errors = validationResult(req)
+    const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() })
+      return res.status(400).json({ errors: errors.array() });
     }
 
     try {
-      const user = await User.findById(req.user.id).select('-passward')
-      const post = await Post.findById(req.params.id)
+      const user = await User.findById(req.user.id).select('-passward');
+      const post = await Post.findById(req.params.id);
 
       const newComment = new Post({
         text: req.body.text,
         name: user.name,
         avatar: user.avatar,
         user: req.user.id
-      })
+      });
 
-      post.comments.unshift(newComment)
+      post.comments.unshift(newComment);
 
-      await post.save()
+      await post.save();
 
-      res.json(post.comments)
+      res.json(post.comments);
     } catch (err) {
-      console.error(err.message)
-      res.status(500).send('Server error')
+      console.error(err.message);
+      res.status(500).send('Server error');
     }
   }
-)
+);
 
 router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
   try {
-    const post = await Post.findById(req.params.id)
+    const post = await Post.findById(req.params.id);
 
     const commentPost = post.comments.find(
       comment => comment.id === req.params.comment_id
-    )
+    );
 
     if (!commentPost) {
-      return res.status(404).json({ msg: 'Comment does not exist' })
+      return res.status(404).json({ msg: 'Comment does not exist' });
     }
 
     if (commentPost.user.toString() !== req.user.id) {
-      return res.status(401).json({ msg: 'User not authorized' })
+      return res.status(401).json({ msg: 'User not authorized' });
     }
 
     const removeIndex = post.comments
       .map(comment => comment.user.toString())
-      .indexOf(req.user.id)
+      .indexOf(req.user.id);
 
-    post.comments.splice(removeIndex, 1)
+    post.comments.splice(removeIndex, 1);
 
-    await post.save()
+    await post.save();
 
-    res.json(post.comments)
+    res.json(post.comments);
   } catch (err) {
-    console.error(err.message)
-    res.status(500).send('Sever error')
+    console.error(err.message);
+    res.status(500).send('Sever error');
   }
-})
+});
 
-module.exports = router
+module.exports = router;
