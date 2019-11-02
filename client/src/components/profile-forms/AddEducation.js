b5c25e520f3bf9b6fc4d9f992ac37a4fc9c03b62
MarkusTryban <markustryban@gmail.com>
2019-11-01 20:37:26 -0400

    Add eslint no-shadow

--- client/src/components/profile-forms/AddEducation.js
+++ client/src/components/profile-forms/AddEducation.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addEducation } from '../../actions/profile';
 
+// eslint-disable-next-line no-shadow
 const AddEducation = ({ addEducation, history }) => {
   const [formData, setFormData] = useState({
     school: '',
