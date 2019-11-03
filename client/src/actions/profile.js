069eaa4f82934796a5314b4aaa69b0f95b407365
MarkusTryban <markustryban@gmail.com>
2019-11-02 21:09:15 -0400

    Delete user account & profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 import { setAlert } from './alert';
 
-import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from './types';
+import {
+  GET_PROFILE,
+  PROFILE_ERROR,
+  UPDATE_PROFILE,
+  CLEAR_PROFILE,
+  ACCOUNT_DELETED
+} from './types';
 
 // Get current user profile
 export const getCurrentProfile = () => async dispatch => {
@@ -143,6 +149,7 @@ export const deleteExperience = id => async dispatch => {
     });
   }
 };
+
 // Delete education
 export const deleteEducation = id => async dispatch => {
   try {
@@ -159,5 +166,24 @@ export const deleteEducation = id => async dispatch => {
       type: PROFILE_ERROR,
       payload: { msg: err.response.statusText, status: err.response.status }
     });
+  }
+};
+
+// Delete account & profile
+export const deleteAccount = () => async dispatch => {
+  if (window.confirm('Are you sure? This can NOT be undone!')) {
+    try {
+      const res = await axios.delete(`/api/profile`);
+
+      dispatch({ type: CLEAR_PROFILE });
+      dispatch({ type: ACCOUNT_DELETED });
+
+      dispatch(setAlert('Your account has been removed'));
+    } catch (err) {
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
   }
 };
