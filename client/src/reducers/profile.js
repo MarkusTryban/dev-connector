d9018d52b27b24ef252150a15048e0aae9b87049
MarkusTryban <markustryban@gmail.com>
2019-11-04 20:15:42 -0500

    Add new case for GET_PROFILES

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,5 +1,6 @@
 import {
   GET_PROFILE,
+  GET_PROFILES,
   PROFILE_ERROR,
   CLEAR_PROFILE,
   UPDATE_PROFILE
@@ -19,6 +20,12 @@ export default function(state = initialState, action) {
   switch (type) {
     case GET_PROFILE:
     case UPDATE_PROFILE:
+      return {
+        ...state,
+        profile: payload,
+        loading: false
+      };
+    case GET_PROFILES:
       return {
         ...state,
         profile: payload,
