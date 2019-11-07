c0bab0c16f8efce80356c95e2913393ddec4c0b7
MarkusTryban <markustryban@gmail.com>
2019-11-06 20:11:01 -0500

    Add eslint no-underscore-dangle

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -25,6 +25,7 @@ const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
           <div className="profiles">
             {profiles.length > 0 ? (
               profiles.map(profile => (
+                // eslint-disable-next-line no-underscore-dangle
                 <ProfileItem key={profile._id} profile={profile} />
               ))
             ) : (
