2fa3d0eebb48b537976631f19d4617e37e386b45
MarkusTryban <markustryban@gmail.com>
2019-11-09 18:13:38 -0500

    Show bio & skills in user profile

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 const ProfileAbout = ({
@@ -7,9 +7,26 @@ const ProfileAbout = ({
     skills,
     user: { name }
   }
-}) => {
-  return <div></div>;
-};
+}) => (
+  <div className="profile-about bg-light p-2">
+    {bio && (
+      <Fragment>
+        <h2 className="text-primary">{name.trim().split(' ')[0]}s Bio</h2>
+        <p>{bio}</p>
+        <div className="line"></div>
+      </Fragment>
+    )}
+
+    <h2 className="text-primary">Skill Set</h2>
+    <div className="skills">
+      {skills.map((skill, index) => (
+        <div key={index} className="p-1">
+          <i className="fa fa-check" /> {skill}
+        </div>
+      ))}
+    </div>
+  </div>
+);
 
 ProfileAbout.propTypes = {
   profile: PropTypes.object.isRequired
