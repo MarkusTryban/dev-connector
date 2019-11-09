a85f2a7fe019f4b249fbe2dd2739507f562ae23f
MarkusTryban <markustryban@gmail.com>
2019-11-09 17:42:13 -0500

    Add ProfileAbout.js

---
+++ client/src/components/profile/ProfileAbout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ProfileAbout = props => {
+  return <div></div>;
+};
+
+ProfileAbout.propTypes = {};
+
+export default ProfileAbout;
