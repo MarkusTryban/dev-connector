05108c65bbaf8e2ac51821c55f1235651f01f08b
MarkusTryban <markustryban@gmail.com>
2019-11-09 16:12:44 -0500

    Add connect & propTypes

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getProfileById } from '../../actions/profile';
 
 const Profile = props => {
-  return <div></div>;
+  return <div>profile test</div>;
 };
 
-Profile.propTypes = {};
+Profile.propTypes = {
+  getProfileById: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired
+};
 
-export default Profile;
+const mapStateToProps = state => ({
+  profile: state.profile,
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { getProfileById }
+)(Profile);
