82cbceb468f1863de933a758fe3f9bda4cced286
MarkusTryban <markustryban@gmail.com>
2019-11-09 17:39:37 -0500

    Add ProfileTop to show user profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
+import ProfileTop from './ProfileTop';
 
 const Profile = ({
   // eslint-disable-next-line no-shadow
@@ -33,6 +34,9 @@ const Profile = ({
                 Edit Profile
               </Link>
             )}
+          <div className="profile-grid my-1">
+            <ProfileTop profile={profile} />
+          </div>
         </Fragment>
       )}
     </Fragment>
