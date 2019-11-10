aa3254a44d9c3a9dd1036fec95abbdd75eac5666
MarkusTryban <markustryban@gmail.com>
2019-11-09 20:05:00 -0500

    Show user Github in profile UI

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -9,6 +9,7 @@ import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
 import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
 
 const Profile = ({
   // eslint-disable-next-line no-shadow
@@ -70,6 +71,9 @@ const Profile = ({
                 <h4>No Education credentials</h4>
               )}
             </div>
+            {profile.githubusername && (
+              <ProfileGithub username={profile.githubusername} />
+            )}
           </div>
         </Fragment>
       )}
