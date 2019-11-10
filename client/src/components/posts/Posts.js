07409cc681bbb6d47435556017b4b5178d21209b
MarkusTryban <markustryban@gmail.com>
2019-11-10 08:51:27 -0500

    Add useEffect: call getPosts to fetch user posts

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getPosts } from '../../actions/post';
 
-const Posts = props => {
+const Posts = ({ getPosts, post: { posts, loading } }) => {
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+
   return <div></div>;
 };
 
