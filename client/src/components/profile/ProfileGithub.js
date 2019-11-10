dbf80075a8fc1f09878faa83cc0d1c0bdd768ab4
MarkusTryban <markustryban@gmail.com>
2019-11-10 07:59:31 -0500

    Show user Github repos in profile

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
 import { getGithubRepos } from '../../actions/profile';
 
 const ProfileGithub = ({ username, getGithubRepos, repos }) => {
@@ -8,7 +9,30 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
     getGithubRepos(username);
   }, [getGithubRepos, username]);
 
-  return <div></div>;
+  return (
+    <div className="profile-github">
+      <h2 className="text-primary m-1">Github Repos</h2>
+      {repos === null ? (
+        <Spinner />
+      ) : (
+        repos.map(repo => (
+          <div key={repo._id} className="repo bg-white p-1 my-1">
+            <div>
+              <h4>
+                <a
+                  href={repo.html_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {repo.name}
+                </a>
+              </h4>
+            </div>
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
 
 ProfileGithub.propTypes = {
