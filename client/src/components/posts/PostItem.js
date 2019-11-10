8f1e9bc37e92d956b84434f229f3f5bfdb8d9bc4
MarkusTryban <markustryban@gmail.com>
2019-11-10 18:09:10 -0500

    Add connect & propTypes

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 export const PostItem = () => {
   return <div></div>;
 };
 
-export default PostItem;
+PostItem.propTypes = {
+  post: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { PostItem }
+)(PostItem);
