2cd22de3357daa0749a782bd151573d353f41bd5
MarkusTryban <markustryban@gmail.com>
2019-11-11 21:13:00 -0500

    Replace postId with id

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -19,13 +19,13 @@ export const getPosts = () => async dispatch => {
 };
 
 // Add like to post
-export const addLike = postId => async dispatch => {
+export const addLike = id => async dispatch => {
   try {
-    const res = await axios.put(`/api/posts/like/${postId}`);
+    const res = await axios.put(`/api/posts/like/${id}`);
 
     dispatch({
       type: UPDATE_LIKES,
-      payload: { postId, likes: res.data }
+      payload: { id, likes: res.data }
     });
   } catch (err) {
     dispatch({
@@ -36,13 +36,13 @@ export const addLike = postId => async dispatch => {
 };
 
 // Remove like
-export const removeLike = postId => async dispatch => {
+export const removeLike = id => async dispatch => {
   try {
-    const res = await axios.put(`/api/posts/unlike/${postId}`);
+    const res = await axios.put(`/api/posts/unlike/${id}`);
 
     dispatch({
       type: UPDATE_LIKES,
-      payload: { postId, likes: res.data }
+      payload: { id, likes: res.data }
     });
   } catch (err) {
     dispatch({
