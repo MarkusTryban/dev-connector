d624b8e1fdf5ddf2877467fc8f8e247b7877b772
MarkusTryban <markustryban@gmail.com>
2019-11-14 18:59:27 -0500

    Add PostForm to Posts.js

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getPosts } from '../../actions/post';
 import PostItem from './PostItem';
+import PostForm from './PostForm';
 
 // eslint-disable-next-line no-shadow
 const Posts = ({ getPosts, post: { posts, loading } }) => {
@@ -17,6 +18,7 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
     <Fragment>
       <h1 className='large text-primary'>Posts</h1>
       <p className='fas fa-user'> Welcome to the community</p>
+      <PostForm />
       <div className='posts'>
         {posts.map(post => (
           // eslint-disable-next-line no-underscore-dangle
