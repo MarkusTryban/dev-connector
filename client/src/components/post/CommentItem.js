2cc4d828d20a6533f4407a30de8eaf0e56ef2573
MarkusTryban <markustryban@gmail.com>
2019-11-17 06:51:32 -0500

    Show user with comment on another users post

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import Moment from 'react-moment';
 
 const CommentItem = ({
   id,
   comment: { _id, text, name, avatar, user, date },
   auth
-}) => {
-  return <div></div>;
-};
+}) => (
+  <div className='post bg-white p-1 my-1'>
+    <div>
+      <Link to={`/profile/${user}`}>
+        <img className='round-img' src={avatar} alt='' />
+        <h4>{name}</h4>
+      </Link>
+    </div>
+    <div>
+      <p className='my-1'>{text}</p>
+      <p className='post-date'>
+        Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>{' '}
+      </p>
+    </div>
+  </div>
+);
 
 CommentItem.propTypes = {
   id: PropTypes.number.isRequired,
