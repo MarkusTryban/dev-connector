048c36958ecc44d01eb6ef39a143bf885cb28c37
MarkusTryban <markustryban@gmail.com>
2019-11-17 16:54:07 -0500

    Moved all routes from App.js to Routes.js

--- client/src/components/layout/NotFound.js
+++ client/src/components/layout/NotFound.js
@@ -6,7 +6,6 @@ export const NotFound = () => {
       <h1 className='x-large text-primary'>
         <i className='fas fa-exclamation-triangle' /> Page Not Found
       </h1>
-      <p className='large'>Page does not exist</p>
     </Fragment>
   );
 };
