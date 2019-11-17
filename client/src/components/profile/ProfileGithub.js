2132de374360f0b5e8edd7c8e948a5e8c4d8c20d
MarkusTryban <markustryban@gmail.com>
2019-11-17 18:47:16 -0500

    Prettier formatting

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -11,20 +11,20 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
   }, [getGithubRepos, username]);
 
   return (
-    <div className="profile-github">
-      <h2 className="text-primary m-1">Github Repos</h2>
+    <div className='profile-github'>
+      <h2 className='text-primary m-1'>Github Repos</h2>
       {repos === null ? (
         <Spinner />
       ) : (
         repos.map(repo => (
           // eslint-disable-next-line no-underscore-dangle
-          <div key={repo._id} className="repo bg-white p-1 my-1">
+          <div key={repo._id} className='repo bg-white p-1 my-1'>
             <div>
               <h4>
                 <a
                   href={repo.html_url}
-                  target="_blank"
-                  rel="noopener noreferrer"
+                  target='_blank'
+                  rel='noopener noreferrer'
                 >
                   {repo.name}
                 </a>
@@ -33,13 +33,13 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
             </div>
             <div>
               <ul>
-                <li className="badge badge-primary">
+                <li className='badge badge-primary'>
                   Stars: {repo.stargazers_count}
                 </li>
-                <li className="badge badge-dark">
+                <li className='badge badge-dark'>
                   Watchers: {repo.watchers_count}
                 </li>
-                <li className="badge badge-light">Forks: {repo.forks_count}</li>
+                <li className='badge badge-light'>Forks: {repo.forks_count}</li>
               </ul>
             </div>
           </div>
