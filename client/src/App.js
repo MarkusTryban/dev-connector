91906b04f92a86d085a83a2dd4979ee25a69306f
MarkusTryban <markustryban@gmail.com>
2019-11-17 16:09:58 -0500

    Add public route for the NotFound component

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import Posts from './components/posts/Posts';
 import Post from './components/post/Post';
+import NotFound from './components/layout/NotFound';
 import PrivateRoute from './components/routing/PrivateRoute';
 import store from './store';
 import { loadUser } from './actions/auth';
@@ -69,6 +70,7 @@ const App = () => {
               />
               <PrivateRoute exact path='/posts' component={Posts} />
               <PrivateRoute exact path='/posts/:id' component={Post} />
+              <Route component={NotFound} />
             </Switch>
           </section>
         </Fragment>
