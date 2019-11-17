615d4357bd4263d2b9b792bf66766472dba5f117
MarkusTryban <markustryban@gmail.com>
2019-11-17 16:25:36 -0500

    Add eslint import/no-named-as-default

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import Posts from './components/posts/Posts';
 import Post from './components/post/Post';
+// eslint-disable-next-line import/no-named-as-default
 import NotFound from './components/layout/NotFound';
 import PrivateRoute from './components/routing/PrivateRoute';
 import store from './store';
